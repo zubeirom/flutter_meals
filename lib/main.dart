238a5d91cb4620ccad2e356f1090339631d49819
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-01-17 02:09:42 +0100

    awesome way of passing data to route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meals/categories_screen.dart';
+import './categories_screen.dart';
+import 'package:flutter_meals/category_meals_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,17 +11,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-          primarySwatch: Colors.pink,
-          accentColor: Colors.amber,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: TextTheme(
-              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              headline1: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-      home: CategoriesScreen(),
-    );
+        title: 'DeliMeals',
+        theme: ThemeData(
+            primarySwatch: Colors.pink,
+            accentColor: Colors.amber,
+            canvasColor: Color.fromRGBO(255, 254, 229, 1),
+            fontFamily: 'Raleway',
+            textTheme: TextTheme(
+                bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                headline1:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+        home: CategoriesScreen(),
+        routes: {'/category-meals': (ctx) => CategoryMealsScreen()});
   }
 }
