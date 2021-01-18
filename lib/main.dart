50aa31e5333b1ee41440fb37cd8316d70ba72b8b
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-01-19 00:27:13 +0100

    add fallback urls

--- lib/main.dart
+++ lib/main.dart
@@ -12,22 +12,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'DeliMeals',
-        theme: ThemeData(
-            primarySwatch: Colors.pink,
-            accentColor: Colors.amber,
-            canvasColor: Color.fromRGBO(255, 254, 229, 1),
-            fontFamily: 'Raleway',
-            textTheme: TextTheme(
-                bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-                bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-                headline1:
-                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-        initialRoute: '/',
-        routes: {
-          '/': (ctx) => CategoriesScreen(),
-          CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
-          MealDetailScreen.routeName: (ctx) => MealDetailScreen()
-        });
+      title: 'DeliMeals',
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: TextTheme(
+              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              headline1: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+      initialRoute: '/',
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen()
+      },
+      // onGenerateRoute: (settings) {
+      //   print(settings.arguments);
+      //   return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      // },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
+    );
   }
 }
