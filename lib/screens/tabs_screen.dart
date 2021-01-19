38ff3203baac316b341b8798264ee0b92dff053a
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-01-19 01:18:05 +0100

    refactor to bottom bar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,28 +8,42 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  List<Map<String, dynamic>> _pages = [
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavouritesScreen(), 'title': 'Favourites'}
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-              title: Text('Meals'),
-              bottom: TabBar(
-                tabs: [
-                  Tab(
-                    icon: Icon(Icons.category),
-                    text: 'Categories',
-                  ),
-                  Tab(
-                    icon: Icon(Icons.star),
-                    text: 'Favourites',
-                  ),
-                ],
-              )),
-          body: TabBarView(
-            children: [CategoriesScreen(), FavouritesScreen()],
-          ),
-        ));
+    return Scaffold(
+      appBar: AppBar(title: Text(_pages[_selectedPageIndex]['title'])),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.category),
+              label: 'Categories'),
+          BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.star),
+              label: 'Favourites')
+        ],
+      ),
+    );
   }
 }
