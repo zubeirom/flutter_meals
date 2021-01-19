deac12abb368d134f902bce11adc14c7fb6663ee
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-01-19 01:57:37 +0100

    replace screens

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meals/widgets/main_drawer.dart';
 
 class FilterScreen extends StatelessWidget {
   static const routeName = '/filters';
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Filter!'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Your filters')),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('Filter!'),
+      ),
     );
   }
 }
